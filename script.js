fd774e4a3eb41dfe84da013ad12a3e9df5f11169
SHARATH SHETTY <130232996+Sharath-shetty28@users.noreply.github.com>
2023-08-03 12:28:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let taskbar=document.getElementsByClassName("taskbar")[0]
 let stratmenu=document.getElementsByClassName("stratmenu")[0]
 taskbar.addEventListener("click",()=>{
-    console.log("clcll")
+    console.log("clicked the window icon")
     if(stratmenu.style.bottom ==="50px"){
         stratmenu.style.bottom="-650px"
     }
